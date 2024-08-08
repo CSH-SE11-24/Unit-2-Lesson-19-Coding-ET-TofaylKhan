921a88dcf4e9bb1ebcd07dcbe4facd55c62b78eb
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 19:14:58 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const prompt = require('prompt-sync')()
+// REMINDER: Run your code to make sure there are no errors before submitting!
+// Create an object that represents your favorite show with the following keys: name (string), genre (string), rating (number) (2 pts)
+let favshow = {
+  name:"stranger thing",
+  genre:"scary",
+  ratings:7
+} 
+
+
+// Console log the show's name using the object (1 pt)
+console.log(name)
+
+
+
+// Add a new key called watched and put in a boolean with whether you have seen it (1 pt)
+favshow['watched'] = 'tvd'
+
+
+
+// Change the rating of the show to increase it by one (2 pt)
+ratings
+i++
+
+
+
+// Loop through the object and print every key and element. (2 pt)
+// It should look like: 
+// name is Ted Lasso
+// genre is comedy
+// rating is 4.5
+// watched is true
+console.log(name)
+console.log(genre)
+console.log(ratings)
+
+
+
+// EXTRA CREDIT
+// Make two more show objects and put them all in an array
+// Loop through the array and print the names of the shows
+
+
+
+
+
